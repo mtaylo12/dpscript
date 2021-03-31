f376e6a13e666b8c694f77bfb6ee9479716c0147
mtaylo12 <70495835+mtaylo12@users.noreply.github.com>
2021-03-30 20:08:12 -0400

    script version one, creates tmp files for dp inputs but doesn't actually call dp and doesn't deal with planar embedding

---
+++ redistscript.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+import networkx as nx
+import sys
+import argparse
+
+def createGraph(c, e, p):
+    G = nx.Graph()
+    G.add_nodes_from(c)
+    G = nx.convert_node_labels_to_integers(G, first_label=0, ordering='default', label_attribute='coords')
+
+    popdict = dict(zip(G.nodes(), p))
+    nx.set_node_attributes(G,popdict, 'pop')
+    G.add_edges_from(e)
+    
+    return G
+
+
+def main():
+    print(sys.argv)
+    assert(len(sys.argv) == 4)
+
+    coordinate_file = sys.argv[1]
+    population_file = sys.argv[2]
+    edge_file = sys.argv[3]
+
+    coordinates = ([(int(x.split()[0]),int(x.split()[1])) for x in open(coordinate_file).readlines()])
+    edges = ([(int(x.split()[0]),int(x.split()[1])) for x in open(edge_file).readlines()])
+    populations = ([int(x.split()[0]) for x in open(population_file).readlines()])
+
+    G = createGraph(coordinates, edges, populations)
+    
+    original_stdout = sys.stdout # Save a reference to the original standard output
+
+    with open('example.co', 'w+') as f, open('examplePops','w+') as p:
+        # f (.co file): node coordinates (row = node index)
+        # p: populations corresponding to node indices
+
+        for n in G:
+            # setup .co file
+            sys.stdout = f
+            coords=nx.get_node_attributes(G,'coords')
+            print(coords[n][0], coords[n][1])
+
+            # setup pop file
+            sys.stdout = p
+            pop=nx.get_node_attributes(G,'pop')
+            print(pop[n])
+
+    with open('example.gr','w+') as g, open('exampleDarts','w+') as d:
+        # g (.gr file): edge descriptions (row = edge index, numbers correspond to vertices)
+        darts = [None]*G.number_of_nodes()
+        for i, e in enumerate(G.edges):
+            # setup edge file
+            sys.stdout = g
+            print(e[0],e[1])
+
+            # setup dart data
+            sys.stdout = d
+            d1 = 2*i
+            d2 = 2*i + 1
+            d1_index = e[0]
+            d2_index = e[1]
+
+            if darts[d1_index] == None:
+                darts[d1_index] = d1
+            if darts[d2_index] == None:
+                darts[d2_index] = d2
+
+        print(*darts, sep = "\n")
+
+            
+    sys.stdout = original_stdout # Reset the standard output to its original value
+
+if __name__ == '__main__':
+    main()
